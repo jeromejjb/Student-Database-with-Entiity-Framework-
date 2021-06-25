0647479d770b26a24f1a6136de8683c97f7cc564
jerome <jeromebrown97@gmail.com>
2021-06-24 23:52:59 -0500

    inital commit for student db with ef

---
+++ Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace StudentDbwithEntity.Models
+{
+    public partial class Student
+    {
+        public int Id { get; set; }
+        public string Fname { get; set; }
+        public string Hometown { get; set; }
+        public string Ffood { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,150 @@
+﻿using StudentDbwithEntity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StudentDbwithEntity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DbStudentsContext db = new DbStudentsContext();
+            MainDisplay(db);
+        }
+
+        public static void MainDisplay(DbStudentsContext db)
+        {
+            bool goOn = true;
+            while (goOn == true)
+            {
+                Console.WriteLine("Welcome to our C# Class");
+                Console.WriteLine("What would you like to do?");
+                Console.WriteLine("1) Learn more about a current student"); 
+                Console.WriteLine("2) Register a new student");
+                int choice = GetInteger(2);
+
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Students in the class: (enter a number 1-12)");
+                        PrintClassmatesById(db);
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Register a student");
+                        AddStudent(db);
+                        break;
+                }
+            }
+        }
+
+        public static void PrintClassmatesById(DbStudentsContext db)
+        {
+            List<Student> students = ReadInStudents(db);
+            foreach (Student s in students)
+            {
+                Console.WriteLine($"{s.Id}: {s.Fname}");
+            }
+            Student student = ChooseStudentById(students);
+            PrintStudentInfo(student);
+        }
+
+        public static Student ChooseStudentById(List<Student> students)
+        {
+            Console.WriteLine("Please enter the ID of the Classmate whose info you would like to view:");
+            int max = students.Count();
+            int id = GetInteger(max);
+            List<Student> queryResults = (from student in students
+                                          where student.Id == id
+                                          select student).ToList();
+            Student s = queryResults[0+id];
+            return s;
+        }
+
+        public static List<Student> ReadInStudents(DbStudentsContext db)
+        {
+            List<Student> students = db.Students.ToList();
+            return students;
+        }
+
+        public static void PrintStudentInfo(Student s)
+        {
+
+            Console.WriteLine($"ID: {s.Id} \t Name:  {s.Fname} \t Hometown: {s.Hometown} \t Favorite Food: {s.Ffood}");
+        }
+
+
+        public static int GetInteger(int maxChoices)
+        {
+            string input = "";
+            int output = 0;
+            try
+            {
+                input = Console.ReadLine();
+                output = int.Parse(input);
+                if (output > maxChoices || output < 1)
+                {
+                    throw new Exception("That number is out of range. Try again.");
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("That was not a valid input.");
+                output = GetInteger(maxChoices);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                output = GetInteger(maxChoices);
+            }
+            return output;
+        }
+
+
+        public static Student MakeStudent()
+        {
+        Student student = new Student();
+        Console.WriteLine("Enter the new student's name:");
+            student.Fname = Console.ReadLine();
+
+            Console.WriteLine("Enter the new classmate's hometown:");
+            student.Hometown = Console.ReadLine();
+
+            Console.WriteLine("Enter the new classmate's favorite food:");
+            student.Ffood = Console.ReadLine();
+
+            return student; 
+        }
+
+    public static void AddStudent(DbStudentsContext db)
+        {
+            //This is the same as a INSERT statement in SQL 
+            Student newStudent = MakeStudent();
+            db.Students.Add(newStudent);
+            db.SaveChanges();
+            Console.WriteLine($"{newStudent.Fname} has been enrolled in the C# course");
+        }
+
+        public static bool GetContinue()
+        {
+            Console.WriteLine("Would you like to roll again? Y/N");
+            string input = Console.ReadLine();
+            if (input.Trim().ToLower() == "y")
+            {
+                return true;
+            }
+            else if (input.Trim().ToLower() == "n")
+            {
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("I don't understand that input. Please try again.");
+                return GetContinue();
+            }
+        }
+    }
+}
